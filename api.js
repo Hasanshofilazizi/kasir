8789c9f9c7bdfcfb24a09a0c9e07c54cff6a84e0
hasan <hasan@DESKTOP-CJ3QF82>
2024-07-21 18:22:43 +0700

    CURD Input InputProduct

--- api.js
+++ api.js
@@ -30,20 +30,99 @@ app.get('/', (req, res) => {
 app.get('/CardHome', (req, res) => {
     res.render("CardHome")
 })
-
-app.get('/Input', (req, res) => {
-    client.query(`SELECT * FROM public.kasir_barang`, (err, result) => {
-        const users= JSON.parse(JSON.stringify(result))
-        if (err) {
-            res.send(err.message)       
-        } else {
-            // res.send(users)
-            res.render("InputProduct",{users: users, title: "hasan"}) // mengirim hasil
-        }
-    })
+// app.get('/InputProduct', (req, res) => {
+//     const users= JSON.parse(JSON.stringify(res))
+//     res.render("InputProduct",{users: users, title: "hasan"})
+// })
+app.get('/Checkout', (req, res) => {
+    res.render("Checkout")
+})
+app.get('/OrderProduct', (req, res) => {
+    res.render("OrderProduct")
+})
+app.get('/PaymentProduct', (req, res) => {
+    res.render("PaymentProduct")
+})
+app.get('/Register', (req, res) => {
+    res.render("Register")
 })
 
-app.post('/',(req,res)=>{
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/InputProduct', (req, res) => {
+    client.query('SELECT * FROM public.kasir_barang', (err, result) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            const users = result.rows;
+            res.render('InputProduct', { users: users, product: null, title: 'Product Management' });
+        }
+    });
+});
+
+app.post('/saveProduct', (req, res) => {
+    const { id, nama_barang, harga_beli, harga_jual } = req.body;
+    if (id) {
+        // Update existing product
+        client.query(
+            'UPDATE public.kasir_barang SET nama_barang = $1, harga_beli = $2, harga_jual = $3 WHERE id = $4',
+            [nama_barang, harga_beli, harga_jual, id],
+            (err, result) => {
+                if (err) {
+                    res.send(err.message);
+                } else {
+                    res.redirect('/InputProduct');
+                }
+            }
+        );
+    } else {
+        // Insert new product
+        client.query(
+            'INSERT INTO public.kasir_barang (nama_barang, harga_beli, harga_jual) VALUES ($1, $2, $3)',
+            [nama_barang, harga_beli, harga_jual],
+            (err, result) => {
+                if (err) {
+                    res.send(err.message);
+                } else {
+                    res.redirect('/InputProduct');
+                }
+            }
+        );
+    }
+});
+
+app.get('/editProduct/:id', (req, res) => {
+    const productId = req.params.id;
+    client.query('SELECT * FROM public.kasir_barang WHERE id = $1', [productId], (err, result) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            const product = result.rows[0];
+            client.query('SELECT * FROM public.kasir_barang', (err, allProducts) => {
+                if (err) {
+                    res.send(err.message);
+                } else {
+                    const users = allProducts.rows;
+                    res.render('InputProduct', { users: users, product: product, title: 'Product Management' });
+                }
+            });
+        }
+    });
+});
+
+app.get('/deleteProduct/:id', (req, res) => {
+    const productId = req.params.id;
+    client.query('DELETE FROM public.kasir_barang WHERE id = $1', [productId], (err, result) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            res.redirect('/InputProduct');
+        }
+    });
+});
+
+
+app.post('/views',(req,res)=>{
 
     const {nama_barang, harga_beli, harga_jual} = req.body
 
@@ -58,7 +137,7 @@ app.post('/',(req,res)=>{
 
 })
 
-app.put('/:id', (req,res)=>{
+app.put('/views/:id', (req,res)=>{
 
     const {nama_barang, harga_beli, harga_jual} = req.body
 
@@ -71,11 +150,9 @@ app.put('/:id', (req,res)=>{
     } )
 })
 
-app.delete('/:id',(req,res)=>{
+app.delete('/views/:id',(req,res)=>{
 
-    const {nama_barang, harga_beli, harga_jual} = req.body
-
-    client.query((`DELETE kasir_barang WHERE id='${req.params.id}'`),(req,result)=>{
+    client.query((`DELETE FROM kasir_barang WHERE id='${req.params.id}'`),(err,result)=>{
 
         if (!err) {
             res.send('Delete sucsess') //mengirim hasil
